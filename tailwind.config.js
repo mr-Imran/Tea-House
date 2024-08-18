397754f3d4519f6f21ff14c16c6902e85002a0f5
Nizam Uddin Imran <39611124+mr-Imran@users.noreply.github.com>
2024-08-18 18:52:25 +0600

    Update
    Home Page Update, Responsive, Animation

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+            'gradient-start': '#FF8938',
+            'gradient-end': '#F00',
+        }
+      },
+    },
+    plugins: [],
+  }
